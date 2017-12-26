ddc091192206a50f621cd00938cae7bc881a097e
carriesy <769669910@qq.com>
2017-12-26 22:13:52 +0800

    add car test

--- car_test.py
+++ car_test.py
@@ -1,5 +1,8 @@
 # -*- coding:utf-8 -*-
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegressionCV
@@ -9,25 +12,22 @@ from sklearn.preprocessing import label_binarize
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
-    pd.set_option('display.width',300)
-    path='data/car.data'
-    data=pd.read_csv(path,header=None)
-    n_columns=len(data.columns)
+    pd.set_option('display.width', 300)
+    path = 'data/car.data'
+    data = pd.read_csv(path, header=None)
+    n_columns = len(data.columns)
     columns = ['buy', 'maintain', 'doors', 'persons', 'boot', 'safety', 'accept']
-    new_columns=dict(zip(np.arange(n_columns),columns))
-    data.rename(columns=new_columns,inplace=True)
+    new_columns = dict(zip(np.arange(n_columns), columns))
+    data.rename(columns=new_columns, inplace=True)
     print data.head()
-    #one_hot编码
+    # one_hot编码
     x = pd.DataFrame()
     for col in columns[:-1]:
-        t=pd.get_dummies(data[col])
-        t.rename(columns=lambda x:col+'_'+str(x))
-        x=pd.concat((x,t),axis=1)
-    print u'输出x前五行',x.head()
+        t = pd.get_dummies(data[col])
+        t = t.rename(columns=lambda x: col + '_' + str(x))
+        x = pd.concat((x, t), axis=1)
+    print u'输出x前五行', x.head()
     print x.columns
 
-    y=np.array(pd.Categorical(data['accept']).codes)
-
-
+    y = np.array(pd.Categorical(data['accept']).codes)
